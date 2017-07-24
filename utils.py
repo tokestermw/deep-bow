b51603c511424a0969881e0add431194d8687296
Motoki Wu <tokestermw@gmail.com>
2017-07-24 16:12:06 -0700

    move makedirs to utils.py

---
+++ utils.py
@@ -0,0 +1,19 @@
+import os
+
+
+def makedirs(name):
+    """helper function for python 2 and 3 to call os.makedirs()
+       avoiding an error if the directory to be created already exists"""
+
+    import os, errno
+
+    try:
+        os.makedirs(name)
+    except OSError as ex:
+        if ex.errno == errno.EEXIST and os.path.isdir(name):
+            # ignore existing directory
+            pass
+        else:
+            # a different error happened
+            raise
+
