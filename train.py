d69f3fd5ef3322690c953d0514f837016282773e
Motoki Wu <tokestermw@gmail.com>
2017-07-24 15:55:26 -0700

    use click for training

--- train.py
+++ train.py
@@ -6,64 +6,69 @@ import torch
 import torch.optim as O
 import torch.nn as nn
 
-from config import get_args, makedirs
+from utils import makedirs
 from data_generator import get_kaggle_sequential_data
 from model import SequenceClassifier
 
-args = get_args()
 
-vocabs, iters = get_kaggle_sequential_data(
-    batch_size=args.batch_size, device=args.gpu,
-    max_size=args.max_size, min_freq=args.min_freq)
-vocab = vocabs[0]
-train_iter, valid_iter = iters
+def train(config, *args, **kwargs):
 
-config = args
-config.n_embed = len(vocab.vocab)
-config.d_out = 1  # regression
-config.n_cells = config.n_layers
+    vocabs, iters = get_kaggle_sequential_data(
+        batch_size=config.batch_size, device=config.gpu,
+        max_size=config.max_size, min_freq=config.min_freq)
+    vocab = vocabs[0]
+    train_iter, valid_iter = iters
 
-# double the number of cells for bidirectional networks
-if config.birnn:
-    config.n_cells *= 2
+    config.n_embed = len(vocab.vocab)
+    config.d_out = 1  # regression
+    config.n_cells = config.n_layers
 
-model = SequenceClassifier(config)
+    # double the number of cells for bidirectional networks
+    if config.birnn:
+        config.n_cells *= 2
 
-criterion = nn.MSELoss()
-opt = O.Adam(model.parameters(), lr=args.lr)
+    model = SequenceClassifier(config)
 
-iterations = 0
-start = time.time()
-makedirs(args.save_path)
+    criterion = nn.MSELoss()
+    opt = O.Adam(model.parameters(), lr=config.lr)
+
+    iterations = 0
+    start = time.time()
+    makedirs(config.save_path)
+
+    for epoch in range(config.epochs):
+        train_iter.init_epoch()
+        for batch_idx, batch in enumerate(train_iter):
+            model.train(); opt.zero_grad()
+
+            iterations += 1
+
+            # forward pass
+            scores = model(batch)
+
+            # calculate loss of the network output with respect to training labels
+            loss = criterion(scores, batch.score)
+
+            # backpropagate and update optimizer learning rate
+            loss.backward(); opt.step()
+
+            # checkpoint model periodically
+            if iterations % config.save_every == 0:
+                snapshot_prefix = os.path.join(config.save_path, 'snapshot')
+                snapshot_path = snapshot_prefix + \
+                    '_loss_{:.6f}_iter_{}_model.pt'.format(loss.data[0], iterations)
+                torch.save(model, snapshot_path)
+                for f in glob.glob(snapshot_prefix + '*'):
+                    if f != snapshot_path:
+                        os.remove(f)
+
+            elif iterations % config.log_every == 0:
+                # print progress message
+                print('Time: {:>2.0f}, Epochs: {}, Iterations: {}, Loss: {:>2.2f}'.format(
+                    time.time()-start, epoch, iterations, loss.data[0]))
 
 
-for epoch in range(args.epochs):
-    train_iter.init_epoch()
-    for batch_idx, batch in enumerate(train_iter):
-        model.train(); opt.zero_grad()
-
-        iterations += 1
-
-        # forward pass
-        scores = model(batch)
-
-        # calculate loss of the network output with respect to training labels
-        loss = criterion(scores, batch.score)
-
-        # backpropagate and update optimizer learning rate
-        loss.backward(); opt.step()
-
-        # checkpoint model periodically
-        if iterations % args.save_every == 0:
-            snapshot_prefix = os.path.join(args.save_path, 'snapshot')
-            snapshot_path = snapshot_prefix + \
-                '_loss_{:.6f}_iter_{}_model.pt'.format(loss.data[0], iterations)
-            torch.save(model, snapshot_path)
-            for f in glob.glob(snapshot_prefix + '*'):
-                if f != snapshot_path:
-                    os.remove(f)
-
-        elif iterations % args.log_every == 0:
-            # print progress message
-            print('Time: {:>2.0f}, Epochs: {}, Iterations: {}, Loss: {:>2.2f}'.format(
-                time.time()-start, epoch, iterations, loss.data[0]))
+if __name__ == '__main__':
+    from config import train_template, cli, SmallConfig
+    train_template(train)
+    cli(obj=SmallConfig)
